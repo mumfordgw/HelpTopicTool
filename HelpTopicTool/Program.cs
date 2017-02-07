f4b3babc0d5f0303b4040749bb37f94f39c359a3
Graham Mumford <graham.mumford@uk.fujitsu.com>
2017-02-07 09:55:06 +0000

    First working dev version

--- HelpTopicTool/Program.cs
+++ HelpTopicTool/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace HelpTopicTool
 {
@@ -15,6 +16,8 @@ namespace HelpTopicTool
             string[] lines = {""};
             bool quiet = false;
             bool debug = false;
+            System.IO.StreamWriter mapFile, aliasFile, renameFile;
+            string currentPath = "";
 
 
             // command line arguments
@@ -31,6 +34,16 @@ namespace HelpTopicTool
                 }
             }
 
+            // get the current directory
+            try
+            {
+                currentPath = Directory.GetCurrentDirectory();
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine("{0} Error encountered getting current directory", Ex);
+            }
+
             // get the filename and read the file into the lines array
             try
             {
@@ -43,6 +56,42 @@ namespace HelpTopicTool
                 Console.WriteLine("{0} Error encountered opening and reading input file {1}", Ex, fileName);
             }
 
+            // Open the output files
+
+            // map file
+            try
+            {
+                mapFile = new System.IO.StreamWriter(@"C:\Users\mumfordgw\Documents\Synchronised Files\VME\vcon\testmap.h");
+            }
+            catch (Exception Ex)
+            {
+                Console.Write("{0} Error encountered opening new map file {1}", Ex, "xxxx");
+                return;
+            }
+
+            // alias file
+            try
+            {
+                aliasFile = new System.IO.StreamWriter(@"C:\Users\mumfordgw\Documents\Synchronised Files\VME\vcon\testalias.txt");
+            }
+            catch (Exception Ex)
+            {
+                Console.Write("{0} Error encountered opening new alias file {1}", Ex, "xxxx");
+                return;
+            }
+
+            // rename entry
+            try
+            {
+                renameFile = new System.IO.StreamWriter(@"C:\Users\mumfordgw\Documents\Synchronised Files\VME\vcon\testrename.txt");
+            }
+            catch (Exception Ex)
+            {
+                Console.Write("{0} Error encountered opening new rename file {1}", Ex, "xxxx");
+                return;
+            }
+
+
             // process each line
             Regex _regex = new Regex(" {2,}");
             string id = "";
@@ -51,15 +100,56 @@ namespace HelpTopicTool
             foreach (string line in lines)
             {
                 //Console.WriteLine("\t" + line);                
+                TopicHandler thisTopic = new TopicHandler();
+
                 string normalisedLine = _regex.Replace(line, " ");
                 char delim = ' ';
                 string[] substrings = normalisedLine.Split(delim);
                 id = substrings[1];
                 file = substrings[2];
                 Console.WriteLine("id={0}, file={1}", id, file);
+                try
+                {
+                    thisTopic.setTopic(id, file);
+                }
+                catch (Exception Ex)
+                {
+                    Console.WriteLine("{0} error encounterd while storing topic {1}", Ex, id);
+                    return;
+                }
+                string newTopicID = thisTopic.setNewTopic();
+                Console.WriteLine("new topic id is " + newTopicID);
+                string newURL = thisTopic.setNewURL();
+                Console.WriteLine("new URL is " + newURL);
+
+                string newMapEntry = thisTopic.getNewMapEntry();
+                Console.WriteLine("new map file entry: " + newMapEntry);
+                mapFile.WriteLine(newMapEntry);
+
+                string newAliasEntry = thisTopic.getNewAliasEntry();
+                Console.WriteLine("new alias file entry: " + newAliasEntry);
+                aliasFile.WriteLine(newAliasEntry);
+
+                string newRenameEntry = thisTopic.getNewRenameEntry();
+                Console.WriteLine("new rename file entry: " + newRenameEntry);
+                renameFile.WriteLine(newRenameEntry);
+
             }
-            //Console.WriteLine("Press any key to exit.");
-            //System.Console.ReadKey();
+
+            // close files
+            // MSDN recommends that the stream is Disposed of rather than flushed and closed.
+            //mapFile.Flush();
+            //mapFile.Close();
+            //aliasFile.Flush();
+            //aliasFile.Close();
+            //renameFile.Flush();
+            //renameFile.Close();
+            mapFile.Dispose();
+            aliasFile.Dispose();
+            renameFile.Dispose();
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
 
 
         }
