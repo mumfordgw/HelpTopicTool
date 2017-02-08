944754d73a4b59beab581cd76b3d5c33de2325a4
Graham Mumford <graham.mumford@uk.fujitsu.com>
2017-02-08 08:05:01 +0000

    Tuesdays fixes

--- HelpTopicTool/Program.cs
+++ HelpTopicTool/Program.cs
@@ -12,11 +12,11 @@ namespace HelpTopicTool
     {
         static void Main(string[] args)
         {
-            string fileName = "";
+            string topicFileName = "";
             string[] lines = {""};
             bool quiet = false;
             bool debug = false;
-            System.IO.StreamWriter mapFile, aliasFile, renameFile;
+            System.IO.StreamWriter mapFile, aliasFile, replaceFile;
             string currentPath = "";
 
 
@@ -24,70 +24,122 @@ namespace HelpTopicTool
             System.Console.WriteLine("Number of command line parameters = {0}", args.Length);
             foreach (string s in args)
             {
+                bool argProcessed = false;
                 if (string.Equals(s, "-q", StringComparison.OrdinalIgnoreCase))
                 {
                     quiet = true;
+                    argProcessed = true;
+                    break;
                 }
                 if (string.Equals(s, "-Debug", StringComparison.OrdinalIgnoreCase))
                 {
                     debug = true;
+                    argProcessed = true;
+                    break;
                 }
+                if (s.Length > 2)
+                {
+                    switch (s.Trim().ToUpper().Substring(0, 3))
+                    {
+                        case "-D:":
+                            currentPath = s.Trim().Substring(3, s.Length - 3);
+                            Console.WriteLine("Directory is {0}", currentPath);
+                            argProcessed = true;
+                            break;
+                        case "-T:":
+                            topicFileName = s.Trim().Substring(3, s.Length - 3);
+                            Console.WriteLine("Topic File is {0}", topicFileName);
+                            argProcessed = true;
+                            break;
+                        default:
+                            Console.WriteLine("Unexpected argument {0}", s);
+                            Console.WriteLine("Usage is!!!!!");
+                            argProcessed = true;
+                            return;
+                    }
+                }
+                if (!argProcessed)
+                {
+                    Console.WriteLine("Unexpected argument {0}", s);
+                    Console.WriteLine("Usage is!!!!!");
+                    return;
+                }
+
             }
 
-            // get the current directory
-            try
+            // check the directory parameter
+            if (currentPath == "")
             {
-                currentPath = Directory.GetCurrentDirectory();
+                Console.WriteLine("ERROR - No -D:directory argument supplied");
+                return;
             }
-            catch (Exception Ex)
+            if (!Directory.Exists(currentPath))
             {
-                Console.WriteLine("{0} Error encountered getting current directory", Ex);
+                Console.WriteLine("ERROR - Directory {0} does not exist");
+                return;
+
             }
+
+            // check the topicFileName parameter
+            if (topicFileName == "")
+            {
+                Console.WriteLine("ERROR - No -T:topicFile argument supplied");
+                return;
+            }
+
+            string topicFileFullName = currentPath + topicFileName;
+            if (!File.Exists(topicFileFullName))
+            {
+                Console.WriteLine("ERROR - Topic File {0} does not exist", topicFileFullName);
+                return;
+
+            }
+
 
             // get the filename and read the file into the lines array
             try
             {
-                if (debug) Console.WriteLine("Enter name of help topic file");
-                // fileName = System.Console.ReadLine();
-                fileName = @"C:\Users\mumfordgw\Documents\Synchronised Files\VME\vcon\topiclistedited.txt";
-                lines = System.IO.File.ReadAllLines(fileName);
+                lines = System.IO.File.ReadAllLines(topicFileFullName);
             } catch (Exception Ex)
             {
-                Console.WriteLine("{0} Error encountered opening and reading input file {1}", Ex, fileName);
+                Console.WriteLine("{0} Error encountered opening and reading input file {1}", Ex, topicFileName);
             }
 
             // Open the output files
 
             // map file
+            string mapFileName = currentPath + "map.h";
             try
             {
-                mapFile = new System.IO.StreamWriter(@"C:\Users\mumfordgw\Documents\Synchronised Files\VME\vcon\testmap.h");
+                mapFile = new System.IO.StreamWriter(mapFileName);
             }
             catch (Exception Ex)
             {
-                Console.Write("{0} Error encountered opening new map file {1}", Ex, "xxxx");
+                Console.Write("{0} Error encountered opening new map file {1}", Ex, mapFileName);
                 return;
             }
 
             // alias file
+            string aliasFileName = currentPath + "alias.h";
             try
             {
-                aliasFile = new System.IO.StreamWriter(@"C:\Users\mumfordgw\Documents\Synchronised Files\VME\vcon\testalias.txt");
+                aliasFile = new System.IO.StreamWriter(aliasFileName);
             }
             catch (Exception Ex)
             {
-                Console.Write("{0} Error encountered opening new alias file {1}", Ex, "xxxx");
+                Console.Write("{0} Error encountered opening new alias file {1}", Ex, aliasFileName);
                 return;
             }
 
-            // rename entry
+            // rename file
+            string replaceFileName = currentPath + "replace.txt";
             try
             {
-                renameFile = new System.IO.StreamWriter(@"C:\Users\mumfordgw\Documents\Synchronised Files\VME\vcon\testrename.txt");
+                replaceFile = new System.IO.StreamWriter(replaceFileName);
             }
             catch (Exception Ex)
             {
-                Console.Write("{0} Error encountered opening new rename file {1}", Ex, "xxxx");
+                Console.Write("{0} Error encountered opening new replace file {1}", Ex, replaceFileName);
                 return;
             }
 
@@ -130,9 +182,9 @@ namespace HelpTopicTool
                 Console.WriteLine("new alias file entry: " + newAliasEntry);
                 aliasFile.WriteLine(newAliasEntry);
 
-                string newRenameEntry = thisTopic.getNewRenameEntry();
-                Console.WriteLine("new rename file entry: " + newRenameEntry);
-                renameFile.WriteLine(newRenameEntry);
+                string newReplaceEntry = thisTopic.getNewRenameEntry();
+                Console.WriteLine("new rename file entry: " + newReplaceEntry);
+                replaceFile.WriteLine(newReplaceEntry);
 
             }
 
@@ -146,7 +198,7 @@ namespace HelpTopicTool
             //renameFile.Close();
             mapFile.Dispose();
             aliasFile.Dispose();
-            renameFile.Dispose();
+            replaceFile.Dispose();
 
             Console.WriteLine("Press any key to exit.");
             System.Console.ReadKey();
